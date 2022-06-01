62db8679aa394a33a9e2009b67fd0150409ff7dd
kunalprakash1309 <kunalprakash1309@gmail.com>
2022-06-01 19:30:24 +0530

    Adding categories to firestore DB

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,16 @@
-import { createContext, useState } from "react";
-import SHOP_DATA from '../shop-data.json'
+import { createContext, useEffect, useState } from "react";
+import SHOP_DATA from '../shop-data.js'
+
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: []
 })
 
 export const ProductProvider = ({children}) => {
-  const [products, setProducts] = useState(SHOP_DATA)
+  const [products, setProducts] = useState([])
+
+
   const value = {products}
   return(
     <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,7 @@ import {
   onAuthStateChanged
 } from 'firebase/auth'
 
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore' 
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from 'firebase/firestore' 
 
 const firebaseConfig = {
   apiKey: "AIzaSyA7XREFGDNJMC0j-JhYQE1jU2zgINs8g10",
@@ -37,6 +37,19 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db)
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object)
+  })
+
+  await batch.commit()
+  console.log('done')
+}
 
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {

fc4a9fe45204c498263c628028dbb9e883c12d38
kunalprakash1309 <kunalprakash1309@gmail.com>
2022-06-21 14:08:45 +0530

    Imporoved app performance using Lazy and suspense on the routing basis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import Authentication from "./routes/authentication/authentication.component";
-import CheckoutPage from "./routes/checkout/checkout.component";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Shop from "./routes/shop/shop.component";
+
+import Spinner from "./components/spinner/spinner.component";
 
 import { checkUserSession } from './store/user/user.actions'
+
+const Home = lazy(() => import("./routes/home/home.component"))
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"))
+const CheckoutPage = lazy(() => import("./routes/checkout/checkout.component"))
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"))
+const Shop = lazy(() => import("./routes/shop/shop.component"))
 
 const App = () => {
 
@@ -18,14 +21,16 @@ const App = () => {
   }, [])
   
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<CheckoutPage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<CheckoutPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
